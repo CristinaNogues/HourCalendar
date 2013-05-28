a85bbad1194c2b2482b15c704b5fb6db81e2eb9f
Theadd <pantallazo@gmail.com>
2013-05-28 19:11:42 +0200

    test

---
+++ HourCalendar/src/hourcalendar/TipusHoresPractica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hourcalendar;
+
+/**
+ *
+ * @author asus
+ */
+public class TipusHoresPractica {
+    public int id;
+    public String nom;
+    
+    public TipusHoresPractica(int _id, String _nom) {
+        id = _id;
+        nom = _nom;
+    }
+}

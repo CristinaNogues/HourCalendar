1d8cbaaa24ef0ae4d2bf93513627ea535293c491
Theadd <pantallazo@gmail.com>
2013-05-28 19:11:22 +0200

    test

---
+++ HourCalendar/src/hourcalendar/Grau.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hourcalendar;
+
+/**
+ *
+ * @author asus
+ */
+public class Grau {
+    public String nom;
+    public String codi;
+
+    public Grau(String _nom, String _codi) {
+        nom = _nom;
+        codi = _codi;
+    }
+}

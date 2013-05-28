7db072e8acc25dfd01590c9f2e21e5afb29226c6
Theadd <pantallazo@gmail.com>
2013-05-28 19:11:55 +0200

    test

---
+++ HourCalendar/src/hourcalendar/TipusMateria.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hourcalendar;
+
+/**
+ *
+ * @author asus
+ */
+public class TipusMateria {
+    public int id;
+    public String nom;
+    
+    public TipusMateria(int _id, String _nom) {
+        id = _id;
+        nom = _nom;
+    }
+}

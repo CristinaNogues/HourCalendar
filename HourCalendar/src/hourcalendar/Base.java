8deff9ca0662064b91f00f585791bb07e8b1a203
Theadd <pantallazo@gmail.com>
2013-05-28 21:13:48 +0200

    Classe Assignatura implementada

--- HourCalendar/src/hourcalendar/Base.java
+++ HourCalendar/src/hourcalendar/Base.java
@@ -13,20 +13,53 @@ import java.util.Vector;
 public class Base {
     private Vector<Assignatura> assignatures;
     private Vector<Grau> graus;
-    public Vector<TipusHoresPractica> tipusHoresPractica;
-    public Vector<TipusMateria> tipusMateria;
+    public Vector<TipusHoresPractica> tipusHoresPractiques;
+    public Vector<TipusMateria> tipusMateries;
     
     public int getNumAssignatures() {
         return assignatures.size();
     }
     
+    /** Retorna una assignatura donada la seva posició dins el vector. **/
     public Assignatura getAssignatura(int idAssignatura) {
         return assignatures.get(idAssignatura);
     }
     
-    public void addAssignatura() {
+    /** Retorna una assignatura donat el seu codi, null si no existeix. */
+    public Assignatura getAssignatura(String codi) {
         
+        int numAssignatures = getNumAssignatures();
+        for (int i = 0; i < numAssignatures; ++i) {
+            Assignatura assignatura = assignatures.get(i);
+            if (assignatura.getCodi().equalsIgnoreCase(codi)) {
+                return assignatura;
+            }
+        }
+        return null;
     }
+    
+    /** Afegeix o modifica una nova assignatura segons el codi d'assignatura.
+     * @return boolean indica si s'ha afegit l'assignatura correctament, no s'afegirà si algun dels paràmetres no és l'esperat
+     **/
+    public boolean addAssignatura(Grau _grau, String _nom, String _codi, TipusMateria _tipus, int _alumnes, int _quadrimestre, int _horesTeoria, int _horesPractica, TipusHoresPractica _tipusHoresPractica) {
+        
+        Assignatura assignatura = new Assignatura(this, _grau, _nom, _codi, _tipus, _alumnes, _quadrimestre, _horesTeoria, _horesPractica, _tipusHoresPractica);
+        
+        if (assignatura.esValida()) {
+            Assignatura aux = getAssignatura(_codi);
+            if (aux != null) {
+                //L'assignatura ja existia, la modifiquem
+                aux = assignatura;
+            } else {
+                //Afegint una nova assignatura
+                assignatures.add(assignatura);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
     
     public int getNumGraus() {
         return graus.size();
@@ -39,5 +72,20 @@ public class Base {
     public void addGrau(String nom, String codi) {
         Grau grau = new Grau(nom, codi);
         graus.add(grau);
+    }
+    
+    /** Retorna cert si existeix l'objecte grau al vector graus. **/
+    public boolean hasGrau(Grau grau) {
+        return graus.contains(grau);
+    }
+    
+    /** Retorna cert si existeix l'objecte TipusHoresPractica al vector tipusHoresPractiques. **/
+    public boolean hasTipusHoresPractica(TipusHoresPractica tipus) {
+        return tipusHoresPractiques.contains(tipus);
+    }
+    
+    /** Retorna cert si existeix l'objecte TipusMateria al vector tipusMateries. **/
+    public boolean hasTipusMateria(TipusMateria tipus) {
+        return tipusMateries.contains(tipus);
     }
 }

eda811fc5dcdda80f77c7cf2144e88927c610705
Theadd <pantallazo@gmail.com>
2013-05-28 19:10:49 +0200

    test

---
+++ HourCalendar/src/hourcalendar/Assignatura.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hourcalendar;
+
+/**
+ *
+ * @author asus
+ */
+public class Assignatura {
+    
+    public Grau grau;
+    public String nom;
+    public String codi;
+    public TipusMateria tipus;
+    public int alumnes;
+    public int curs;
+    public int horesTeoria;
+    public int horesPractica;
+    public TipusHoresPractica tipusHoresPractica;
+    
+}
